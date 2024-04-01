3fed265d4018f8fa513293cb4b006b73a51a57cf
TendingStream73 <sasasabaev679@gmail.com>
2024-04-01 23:28:12 +0300

    src: decompile main

---
+++ src/main.cpp
@@ -0,0 +1,44 @@
+int main(void)
+
+{
+  DeviceManager _gDeviceManager;
+  int counter;
+  
+  __main();
+  SizeMem();
+  DetermineBootType();
+  InitExceptions();
+  InitHeap();
+  InitializeMemoryManagement();
+  InitIICIntf();
+  CrashCounter();
+  InitializeFilesystems();
+  _gVideoDriver = malloc(0x70);
+  VideoDriver::Init();
+  InitCrashLog();
+  SetROMTiming();
+  InitSiliconSerialNumber();
+  _gDeviceManager = new DeviceManager();
+  DeviceManager::ProbeExpansionBus();
+  InitDebugIO();
+  InitRIO();
+  InitWTVPort();
+  InitAudio();
+  counter = FetchCounter();
+  SetCompare(counter + _DAT_8000028c * 10000);
+  enable_cpu_ints(0xc400);
+  SetPowerLEDState(2);
+  InitParallelPort();
+  InitDisk();
+  if (gEraseNVSoon != '\0') {
+    NVErase();
+  }
+  InitIR();
+  InitTuner();
+  _DAT_a4000030 = 0x69;
+  Watchdog(1);
+  NVInit(1);
+  ProtectMemory();
+  LateBoot();
+  return 0;
+}
