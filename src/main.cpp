cfe0e9c80294690a9370b0f2cced078a103a5f4d
TendingStream73 <sasasabaev679@gmail.com>
2024-04-07 18:04:01 +0300

    src: move main to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,50 @@
-int main(void)
+#include "include/webtv.h"
+
+undefined4 main(undefined4 param_1,undefined *param_2,undefined4 param_3,undefined4 *param_4)
 
 {
-  DeviceManager _gDeviceManager;
-  int counter;
-  
-  SizeMem();
-  DetermineBootType();
-  InitExceptions();
-  InitHeap();
-  InitializeMemoryManagement();
-  InitIICIntf();
-  CrashCounter();
-  InitializeFilesystems();
-  _gVideoDriver = malloc(0x70);
-  VideoDriver::Init();
-  InitCrashLog();
-  SetROMTiming();
-  InitSiliconSerialNumber();
-  _gDeviceManager = new DeviceManager();
-  DeviceManager::ProbeExpansionBus();
-  InitDebugIO();
-  InitRIO();
-  InitWTVPort();
-  InitAudio();
-  counter = FetchCounter();
-  SetCompare(counter + _DAT_8000028c * 10000);
-  enable_cpu_ints(0xc400);
-  SetPowerLEDState(2);
-  InitParallelPort();
-  InitDisk();
-  if (gEraseNVSoon != '\0') {
-    NVErase();
-  }
-  InitIR();
-  InitTuner();
-  WDVALUE_0030 = 0x69;
-  Watchdog(1);
-  NVInit(1);
-  ProtectMemory();
-  LateBoot();
-  return 0;
+    uint **in_v0;
+    int iVar1;
+    undefined4 uVar2;
+
+    SizeMem();
+    DetermineBootType();
+    InitExceptions();
+    InitHeap();
+    InitializeMemoryManagement();
+    InitIICIntf();
+    CrashCounter();
+    InitializeFilesystems();
+    __builtin_new(0x70);
+    _gVideoDriver = in_v0;
+    VideoDriver__Init((char *)in_v0);
+    InitCrashLog();
+    SetROMTiming();
+    InitSiliconSerialNumber();
+    __builtin_new(4);
+    DeviceManager__DeviceManager(in_v0);
+    _gDeviceManager = in_v0;
+    DeviceManager__ProbeExpansionBus(in_v0);
+    InitDebugIO();
+    InitRIO();
+    InitWTVPort();
+    InitAudio();
+    iVar1 = FetchCounter();
+    SetCompare(iVar1 + _DAT_8000028c * 10000);
+    enable_cpu_ints(0xc400);
+    SetPowerLEDState(2);
+    InitParallelPort();
+    InitDisk();
+    if (gEraseNVSoon != '\0') {
+        NVErase();
+    }
+    InitIR();
+    InitTuner();
+    _DAT_a4000030 = 0x69;
+    Watchdog('\x01');
+    uVar2 = 1;
+    NVInit(1);
+    ProtectMemory();
+    LateBoot(uVar2,param_2,param_3,param_4);
+    return 0;
 }
