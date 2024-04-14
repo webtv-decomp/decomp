f54c4ce9e8efc6df3310caa68274471da455f51e
TheMsntv2Guy <149720149+TheMsntv2Guy@users.noreply.github.com>
2024-04-14 16:11:38 +0100

    Update InitParallelPort.cpp

--- src/InitParallelPort.cpp
+++ src/InitParallelPort.cpp
@@ -3,28 +3,28 @@ int InitParallelPort(void)
 {
   int syschiprevision;
   ParallelDriver _gParallelDriver;
-  int *piVar3;
-  int iVar4;
+  int BoxParallelHWIntf;
+  int SoloParallelHWIntf;
   
   syschiprevision = SystemChipRevision();
   if (syschiprevision == 0) {
     ParallelDriver = __builtin_new(0x10);
-    piVar3 = (int *)BoxParallelHWIntf::BoxParallelHWIntf(ParallelDriver);
+    BoxParallelHWIntf = (int *)BoxParallelHWIntf::BoxParallelHWIntf(ParallelDriver);
   }
   else {
     ParallelDriver = __builtin_new(0xc);
-    piVar3 = (int *)SoloParallelHWIntf::SoloParallelHWIntf(ParallelDriver);
+    BoxParallelHWIntf = (int *)SoloParallelHWIntf::SoloParallelHWIntf(ParallelDriver);
   }
-  syschiprevision = (**(code **)(*piVar3 + 0x14))((int)piVar3 + (int)*(short *)(*piVar3 + 0x10));
-  iVar4 = 0;
+  syschiprevision = (**(code **)(*BoxParallelHWIntf + 0x14))((int)BoxParallelHWIntf + (int)*(short *)(BoxParallelHWIntf + 0x10));
+  SoloParallelHWIntf = 0;
   if (syschiprevision == 0) {
     _gParallelDriver = __builtin_new(0x1c);
-    _gParallelDriver = ParallelDriver::ParallelDriver(_gParallelDriver,piVar3);
-    iVar4 = 1;
+    _gParallelDriver = ParallelDriver::ParallelDriver(_gParallelDriver,BoxParallelHWIntf);
+    SoloParallelHWIntf = 1;
     if (_gParallelDriver == 0) {
-      __builtin_delete(piVar3);
-      iVar4 = 0;
+      __builtin_delete(BoxParallelHWIntf);
+      SoloParallelHWIntf = 0;
     }
   }
-  return iVar4;
+  return SoloParallelHWIntf;
 }
